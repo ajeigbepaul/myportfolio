799a93fc6dc8ec4177ab3288f3d7a8c1983df2d8
ajeigbepaul <pdave4krist@yahoo.com>
2024-08-21 15:27:34 +0100

    working on my porfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex relative bg-black-100 flex-col items-center justify-between sm:px-10 px-5 mx-auto overflow-hidden">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+        navItems={[
+
+        ]}/>
        <Hero/>
+       <Grid/>
       </div>
     </main>
   );
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
   return (
@@ -15,7 +18,7 @@ const Hero = () => {
         />
         <Spotlight className="top-28 left-80 h-[80vh]" fill="blue" />
       </div>
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
@@ -24,6 +27,17 @@ const Hero = () => {
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web inspiration with Next.js
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Bringing Seamless Experiences to the Web"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">Hi, I am Ajeigbe Paul a Next Js and React Native developer in Lagos, Nigeria.</p>
+          <MagicButton
+          title="Show my work"
+          icon={<FaLocationArrow/>}
+          position="right"
+          
+          />
         </div>
       </div>
     </div>
