44e54d0369756ac5fae7e84cb730df159596eb2d
ajeigbepaul <pdave4krist@yahoo.com>
2024-08-24 16:12:02 +0100

    process section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Process from "@/components/Process";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,11 +13,13 @@ export default function Home() {
     <main className="flex relative bg-black-100 flex-col items-center justify-between sm:px-10 px-5 mx-auto overflow-hidden">
       <div className="max-w-7xl w-full">
         <FloatingNav
-        navItems={[
-
-        ]}/>
+        navItems={navItems}/>
        <Hero/>
        <Grid/>
+       <RecentProjects/>
+       <Clients/>
+       <Experience/>
+       <Process/>
       </div>
     </main>
   );
